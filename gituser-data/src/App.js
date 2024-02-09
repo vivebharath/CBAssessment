7265e64f497fde9eee3e55958adc68d63be34cfc
vivebharath <vivebharath@gmail.com>
2024-02-10 03:58:55 +0530

    Initial commit

--- gituser-data/src/App.js
+++ gituser-data/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import UserList from './UserList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserDetail from './UserDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<UserList />}/>
+     <Route path="user-details/:username" element={<UserDetail/>}/>
+     <Route path="*" element={<h1>NO Page Found</h1>}/>
+     </Routes>
+     </BrowserRouter>
+  )
 }
 
 export default App;
